36aa353150c1c6c8859b341a62b0c52f34851b1b
Florian Le Frioux <florian.lefrioux@espacechange.com>
2014-12-19 19:35:49 +0100

    first dashboard layout

---
+++ config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Huna
+# config.py
+
+from __future__ import unicode_literals
+
+# Flask
+DEBUG = True
+LOG_FILE = 'huna_dashboard.log'
+LOG_FORMAT = '%(asctime)s - %(levelname)s :: %(message)s'
+HOST = '0.0.0.0'
+PORT = 5000 if DEBUG else 8000
+STATIC_FOLDER = '/static'
+
+# Huna
+SITE_NAME = 'Huna Dashboard'
+
+# New Relic
+NEWRELIC_KEY = '87cc8e42ea238a1b3fb6448cecd53f1126ac83e0'
+
+# Cloudera
+CLOUDERA_URL = 'http://clouderaurl.fr'
+CLOUDERA_KEY = '87cc8e42ea238a1b3fb6448cecd53f1126ac83e0'
+
+# Jenkins
+JENKINS_URL = 'http://jenkinsurl.fr'
+JENKINS_KEY = '87cc8e42ea238a1b3fb6448cecd53f1126ac83e0'
---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Huna
+
+from __future__ import unicode_literals
+import logging
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, render_template
+
+# Build Flask instance with its config
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+# Getting a rotating log
+handler = RotatingFileHandler(filename=app.config['LOG_FILE'],
+                              backupCount=10, mode='a', maxBytes=1000000)
+if app.debug:
+    handler.setLevel(logging.DEBUG)
+else:
+    handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+app.logger.addHandler(handler)
+
+
+# Homepage
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+
+# Favicon route
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('img/favicon.ico')
+
+if __name__ == '__main__':
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
