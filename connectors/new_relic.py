1ce801b9e546691ba1390215068fbe60ab1da12f
Florian Le Frioux <florian.lefrioux@espacechange.com>
2015-01-30 18:26:24 +0100

    new plugin WIP: new relic

--- connectors/new_relic.py
+++ connectors/new_relic.py
@@ -5,8 +5,24 @@
 # new_relic.py
 
 from __future__ import unicode_literals
+from collections import Counter
 
 from flask import current_app as app
-from newrelic_api import Applications
+from newrelic_api import Applications, Servers
 
-app = Applications(api_key=app.config['NEWRELIC_KEY'])
+
+class NewRelic():
+
+    def __init__(self, api_key=None):
+        self.api_key = api_key if api_key else app.config.get('NEWRELIC_KEY', None)
+
+    def get_servers_statuses(self):
+        return [{'host': s['host'], 'health_status': s['health_status']}
+                for s in Servers(api_key=self.api_key).list()['servers']]
+
+    def get_apps_statuses(self):
+        return [{'name': s['name'], 'health_status': s['health_status']}
+                for s in Applications(api_key=self.api_key).list()]
+
+    def health_stats(self):
+        return Counter(map(lambda status: status['health_status'], self.get_servers_statuses()))
