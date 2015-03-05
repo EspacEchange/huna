c5b0fda395186cec0a46ad04c785242b8ebd4c2f
Florian Le Frioux <florian.lefrioux@espacechange.com>
2015-03-05 16:21:52 +0100

    fix base_connector args

--- connectors/base_connector.py
+++ connectors/base_connector.py
@@ -12,11 +12,11 @@ class BaseConnector():
 
     def __init__(self, api_key=None, host=None, port=None, username=None, password=None):
         self.success = False
-        self.api_key = None
-        self.host = None
-        self.port = None
-        self.username = None
-        self.password = None
+        self.api_key = api_key if api_key else ''
+        self.host = host if host else ''
+        self.port = port if port else ''
+        self.username = username if username else ''
+        self.password = password if password else ''
 
     def list_of_dict_stats(self, list_of_dict, key):
         return Counter(map(lambda x: x[key], list_of_dict))
